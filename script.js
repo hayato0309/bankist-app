7fd6c5d620e4c492f119d9fa561c76e9ab0edd7f
hayato0309 <hayato.pluto.4538@gmail.com>
2021-10-28 08:31:48 +0900

    #161 Sorting arrays - not done yet

--- script.js
+++ script.js
@@ -387,3 +387,34 @@ console.log(overallBalance);
 // flatMap
 const overallBalance2 = accounts.flatMap(acc => acc.movements).reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance2);
+
+// #161 Sorting arrays
+
+// with Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// with Numbers
+console.log(movements);
+console.log(movements.sort());
+
+// return < 0, A, B (keep order)
+// return > 0, A, B (switch order)
+// movements.sort((a, b) => {
+//   if (a > b)
+//     return 1;
+//   if (a < b)
+//     return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// movements.sort((a, b) => {
+//   if (a > b)
+//     return -1;
+//   if (a < b)
+//     return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
